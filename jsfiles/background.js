a23b1533c695b2ad09194682f9bec7cb2bb0e8a3
Yasmin Carvalho <miojodepopoca@gmail.com>
2021-07-22 12:17:22 -0300

    New portifolio

---
+++ jsfiles/background.js
@@ -0,0 +1,11 @@
+// Mouse 1
+document.getElementById("button1").addEventListener("mouseenter", function(){
+    document.getElementById("background1").classList.add("select");
+});
+document.getElementById("button1").addEventListener("mouseleave", function(){
+    document.getElementById("background1").classList.remove("select");
+});
+
+
+
+
